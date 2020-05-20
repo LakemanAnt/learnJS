f6aa7c0507c94e4ef3463d25f5d27278e8c302e0
Anton <lakeman.ant@gmail.com>
2020-05-20 23:39:16 +0300

    add task 13

--- Task9 - 13/js/script.js
+++ Task9 - 13/js/script.js
@@ -34,7 +34,7 @@ window.addEventListener('DOMContentLoaded', function () {
     })
 
     //Timer
-    let deadline = '2020-05-17'
+    let deadline = '2020-05-25'
 
     function getTimeRemaining(endtime){
         let t = Date.parse(endtime) - Date.parse(new Date())
@@ -99,5 +99,94 @@ window.addEventListener('DOMContentLoaded', function () {
             this.classList.add('more-splash')
             document.body.style.overflow = 'hidden'
         })
+    })
+
+
+    //Form
+
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так('
+    }
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status')
+
+    form.addEventListener('submit', (event)=>{
+        event.preventDefault()
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest()
+
+        request.open('POST', 'server.php')
+        request.setRequestHeader ('Context-Type', 'application/json; charset=utf-8')
+
+        let formData = new FormData(form)
+
+        let obj = {}
+        //перевод формдата в простой обьект
+        formData.forEach(function(value, key){
+            obj[key] = value
+        })
+        let json = JSON.stringify(obj);
+        request.send(json)
+
+        request.addEventListener('readystatechange', ()=>{
+            if (request.readyState < 4 ){
+                statusMessage.innerHTML = message.loading
+            } else if(request.readyState === 4 && request.status == 200){
+                statusMessage.innerHTML = message.success
+            } else {
+                statusMessage.innerHTML = message.failure
+            }
+        })
+
+        for (let i=0; i < input.length; i++){
+            input[i].value = '';
+        }
+    })
+
+    //Login form
+
+    let loginForm = document.querySelector('#form'),
+        inputLogin = loginForm.getElementsByTagName('input');
+    
+    loginForm.addEventListener('submit', (event)=>{
+        event.preventDefault()
+        loginForm.appendChild(statusMessage);
+    
+        let request = new XMLHttpRequest()
+
+        request.open('POST', 'server.php')
+        request.setRequestHeader ('Context-Type', 'application/json; charset=utf-8')
+
+        let formData = new FormData(loginForm)
+
+        let obj = {}
+        //перевод формдата в простой обьект
+        formData.forEach(function(value, key){
+            obj[key] = value
+        })
+        let jsonInfo = JSON.stringify(obj);
+        request.send(jsonInfo)
+
+        request.addEventListener('readystatechange', ()=>{
+            if (request.readyState < 4 ){
+                statusMessage.innerHTML = message.loading
+            } else if(request.readyState === 4 && request.status == 200){
+                statusMessage.innerHTML = message.success
+            } else {
+                statusMessage.innerHTML = message.failure
+            }
+        })
+        console.log(inputLogin.length)
+        for (let i=0; i < inputLogin.length; i++){
+            inputLogin[i].value = '';
+        }
+
     })
 })
